370f7aad6ca4b22c3d48e6b1c7af0d7aa6127310
nsieverywhere <nsikansimon@gmail.com>
2022-11-14 15:45:41 +0100

    remains the cron scheduler

--- index.js
+++ index.js
@@ -3,59 +3,53 @@ const bodyparser = require("body-parser");
 const sqlite3 = require("sqlite3").verbose();
 const axios = require("axios");
 const schedule = require("node-schedule");
+const Datastore = require("nedb");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyparser.urlencoded({ extended: true })); //important for collecting post content
 app.use(express.static("public"));
-let stories = [];
-let id = "";
-let title = "";
-let author = "";
-let content = [];
 
-// create stories DB
-let storiesDB = new sqlite3.Database("./db/storiesdb", (err) => {
-  if (err) {
-    return console.error(err.message);
-  } else {
-    storiesDB.run(
-      "CREATE TABLE IF NOT EXISTS story(author, id, title, content, comments)"
-    );
-    console.log("database created");
-  }
-});
+let stories = [];
+
+const storydb = new Datastore("db/stories.db");
+storydb.loadDatabase();
+
+const commentdb = new Datastore("db/comment.db");
+commentdb.loadDatabase();
+
+// let kids = [];
 
 // getting data and sending to database every 5 min.
 function getData() {
   axios("https://hacker-news.firebaseio.com/v0/topstories.json")
     .then((response) => {
       let result = response.data.slice(0, 1);
+
       result.forEach((id) => {
+        // axios to get the actual content from api
         axios
           .get("https://hacker-news.firebaseio.com/v0/item/" + id + ".json")
           .then((response) => {
-            let item = response.data;
-            id = response.data.id;
-            title = response.data.title;
-            author = response.data.by;
-            stories.push(item);
+            storydb.insert(response.data);
+            // console.log("story inserted");
+            let kids = response.data.kids;
 
-            item.kids.forEach((newitem) => {
+            // code to get comments
+            kids.forEach((id) => {
               axios
                 .get(
-                  "https://hacker-news.firebaseio.com/v0/item/" +
-                    newitem +
-                    ".json"
+                  "https://hacker-news.firebaseio.com/v0/item/" + id + ".json"
                 )
                 .then((response) => {
-                  content = response.data.by;
-                  console.log(content);
+                  commentdb.insert(response.data);
+                  // console.log("comment inserted");
                 })
                 .catch((err) => {
                   console.log(err);
                 });
             });
+            // -----
           })
           .catch((err) => {
             console.log(err);
@@ -65,36 +59,45 @@ function getData() {
     .catch((err) => {
       console.log(err);
     });
-
-  // insert items into db
-  storiesDB.run(
-    `insert into story(id, title, author, content) VALUES(?, ?, ?, ?)`,
-    [id, title, author, content],
-    function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("item inserted in db.");
-      }
-    }
-  );
 }
 
-const job = schedule.scheduleJob("job", "*/10 * * * * *", () => {
+const job = schedule.scheduleJob("job", "*/1 * * * *", () => {
   getData();
-  job.cancel();
+  console.log("content fetched");
 });
 
 app.get("/", async (req, res) => {
-  res.render("index", { stories });
+  storydb.find({}, function (err, output) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { output });
+    }
+  });
 });
 
 app.get("/news", (req, res) => {
   res.render("news");
 });
 
-app.get("/story", (req, res) => {
-  res.render("story");
+app.get("/story/:id", (req, res) => {
+  let parentid = Number(req.params["id"]);
+
+  storydb.find({ id: parentid }, function (err, storycontent) {
+    if (err) {
+      console.log(err);
+    } else {
+      commentdb.find({ parent: parentid }, function (err, commentcontent) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("story", { commentcontent, storycontent });
+          // const d = new Date();
+          // console.log(d);
+        }
+      });
+    }
+  });
 });
 
 //
